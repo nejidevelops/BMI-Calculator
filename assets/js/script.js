376094b13388b2bc7fecbb015e55ee82a587616e
NewtonOmbese <newtonombese1@gmail.com>
2022-07-03 21:43:57 +0300

    got it working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 let myHeight = document.getElementById("heightH")
 let myWeight = document.getElementById("wght")
-let comment = document.getElementById("appendComment")
+let computed = document.getElementById("appendComputed")
 let calculateIt = document.getElementById("calculateB")
+let statusBMI = document.getElementById("appendComment")
 let bMI;
 
 calculateIt.addEventListener("click", computedB)
@@ -10,6 +11,16 @@ function computedB(){
     let heightt = myHeight.value;
     let weightt = myWeight.value;
     bMI = (weightt) / ((heightt ** 2)/10000) 
-    comment.textContent = bMI.toFixed(2)
+    computed.textContent = bMI.toFixed(2)
 
+    if(computed > 24.9){
+        statusBMI.textContent = "Your's is over weight"
+    }
+    else if(computed >= 18.6 && computed <= 24.9){
+        statusBMI.textContent = "Your's is a normal range"
+    }
+    else{
+        statusBMI.textContent = "Your's is under weight"
+    }
 }
+
