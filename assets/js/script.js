9a35fe44bd23b013cbba8a2f201ea2e9f1ed8c1e
NewtonOmbese <newtonombese1@gmail.com>
2022-07-03 22:32:25 +0300

    rectifying it all and getting it deployed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let computed = document.getElementById("appendComputed")
 let calculateIt = document.getElementById("calculateB")
 let statusBMI = document.getElementById("appendComment")
 let clearAndAwait = document.querySelectorAll("input")
+let validator = document.getElementById("validationNotifier")
 let bMI;
 
 calculateIt.addEventListener("click", computedB)
@@ -11,6 +12,9 @@ calculateIt.addEventListener("click", computedB)
 function computedB(){
     let heightt = myHeight.value;
     let weightt = myWeight.value;
+    if(isNaN(heightt && weightt)){
+        validator.textContent = "Only numeric values are allowed"
+    }
     bMI = (weightt) / ((heightt ** 2)/10000) 
     computed.textContent = bMI.toFixed(2)
 
@@ -20,7 +24,7 @@ function computedB(){
     else if(computed >= 18.6 && computed <= 24.9){
         statusBMI.textContent = "Your's is a normal range"
     }
-    else{
+    else if(computed < 18.6){
         statusBMI.textContent = "Your's is a under weight"
     }
     clearAndAwait.forEach(input => input.value = "")
